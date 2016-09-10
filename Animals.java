569a59e55a7e27e4105bd7b60131d03a882d5cb3
Qasim04 <qasimhaider83@yahoo.com>
2016-09-10 17:12:28 +0100

    Create Animals.java

---
+++ Animals.java
@@ -0,0 +1,20 @@
+public class Animals {
+
+	private String species;
+	private String order;
+	public String getSpecies() {
+		return species;
+	}
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+	public String getOrder() {
+		return order;
+	}
+	public void setOrder(String order) {
+		this.order = order;
+	}
+	
+	
+	
+}
